41a94cad7b55787399d40f689beb554f0b4fdc47
danitza18 <danitzamarlenechurasocujayo@gmail.com>
2019-09-14 21:49:43 -0500

    mir primer capa web con MVC

---
+++ src/test/java/almacen_para_cualquier_empresa/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package almacen_para_cualquier_empresa.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
